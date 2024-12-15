c3d2caf7c44fbe57018853f6e05d578c355552c4
professorU <38992986+professorU@users.noreply.github.com>
2024-12-15 14:57:02 +0300

    we started to build something.

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,40 @@
-import 'package:just_audio/just_audio.dart';
-import 'package:audio_service/audio_service.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-
 
 void main() {
   runApp(MyApp());
-  
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: const Text('Andromedia Logosu')),
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('Hello ALperen'),
-          Text(appState.current.asLowerCase),
-          
-          ElevatedButton(
-            onPressed: () {
-              print('Hello World!');
-            },
-            child: Text('Hello World'),
-
-          )
-
+        bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), 
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_fill),
+            label: 'Müzik',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Playlist',
+          ),
         ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            
+          ),
+        ),
         
       ),
     );
